954cc065a5b491be59122601d6dda6a5b62fa8fa
Yuer <passerbyid@gmail.com>
2016-08-29 23:54:16 +0800

    Fix typo.

--- src/logger.js
+++ src/logger.js
@@ -2,7 +2,7 @@ import Moment from 'moment';
 
 class Logger {
   log(text) {
-    const dateText = moment().format( 'YYYY/MM/DD HH:mm:ss')
+    const dateText = Moment().format( 'YYYY/MM/DD HH:mm:ss')
     console.log(`${dateText}: ${text}`);
   }
 }
